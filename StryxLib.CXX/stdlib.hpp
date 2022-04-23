7cf673876d69d3d2c3230db46aac808261271196
Connor Shearer <connor_shearer@outlook.com>
2022-04-23 22:13:03 +0100

    Dont use the plain CMake project, use solution projects instead

--- StryxLib.CXX/stdlib.hpp
+++ StryxLib.CXX/stdlib.hpp
@@ -2,35 +2,35 @@
 #ifndef __STDLIBCXX__
 #define __STDLIBCXX__
 // Concepts library
-#include <concepts> // (C++20) Fundamental library concepts
+// NOT SUPPORTED BY LINUX | #include <concepts> // (C++20) Fundamental library concepts
 // Coroutines library
-#include <coroutine> // (C++20) Coroutine support library
+// NOT SUPPORTED BY LINUX | #include <coroutine> // (C++20) Coroutine support library
 // Utilities library
-#include <any> // (C++17) std::any class
+// NOT SUPPORTED BY LINUX | #include <any> // (C++17) std::any class
 #include <bitset> // std::bitset class template
 #include <chrono> // (C++11) C++ time utilites
-#include <compare> // (C++20) Three - way comparison operator support
+// NOT SUPPORTED BY LINUX | #include <compare> // (C++20) Three - way comparison operator support
 #include <csetjmp> // Macro(and function) that saves(and jumps) to an execution context
 #include <csignal> // Functions and macro constants for signal management
 #include <cstdarg> // Handling of variable length argument lists
 #include <cstddef> // Standard macrosand typedefs
 #include <cstdlib> // General purpose utilities : program control, dynamic memory allocation, random numbers, sortand search
 #include <ctime> // C - style time / date utilites
-//#include <expected> // (C++23) std::expected class template
+// NOT SUPPORTED BY EITHER | #include <expected> // (C++23) std::expected class template
 #include <functional> // Function objects, Function invocations, Bind operationsand Reference wrappers
 #include <initializer_list> // (C++11) std::initializer_list class template
-#include <optional> // (C++17) std::optional class template
-#include <source_location> // (C++20) Supplies means to obtain source code location
+// NOT SUPPORTED BY LINUX | #include <optional> // (C++17) std::optional class template
+// NOT SUPPORTED BY LINUX | #include <source_location> // (C++20) Supplies means to obtain source code location
 #include <tuple> // (C++11) std::tuple class template
 #include <type_traits> // (C++11) Compile - time type information
 #include <typeindex> // (C++11) std::type_index
 #include <typeinfo> // Runtime type information utilities
 #include <utility> // Various utility components
-#include <variant> // (C++17) std::variant class template
-#include <version> // (C++20) Supplies implementation - dependent library information
+// NOT SUPPORTED BY LINUX | #include <variant> // (C++17) std::variant class template
+// NOT SUPPORTED BY LINUX | #include <version> // (C++20) Supplies implementation - dependent library information
 // - Dynamic memory management
 #include <memory> // High - level memory management utilities
-#include <memory_resource> // (C++17) Polymorphic allocators and memory resources
+// NOT SUPPORTED BY LINUX | #include <memory_resource> // (C++17) Polymorphic allocators and memory resources
 #include <new> // Low - level memory management utilities
 #include <scoped_allocator> // (C++11) Nested allocator class
 // - Numeric limits
@@ -45,17 +45,17 @@
 #include <exception> // Exception handling utilities
 #include <stdexcept> // Standard exception objects
 #include <system_error> // (C++11) Defines std::error_code, a platform - dependent error code
-//#include <stacktrace> // (C++23) Stacktrace library 
+// NOT SUPPORTED BY EITHER | #include <stacktrace> // (C++23) Stacktrace library 
 // Strings library
 #include <cctype> // Functions to determine the category of narrow characters
-#include <charconv> // (C++17) std::to_chars and std::from_chars
+// NOT SUPPORTED BY LINUX | #include <charconv> // (C++17) std::to_chars and std::from_chars
 #include <cstring> // Various narrow character string handling functions
-#include <cuchar> // (C++11) C - style Unicode character conversion functions
+// NOT SUPPORTED BY LINUX | #include <cuchar> // (C++11) C - style Unicode character conversion functions
 #include <cwchar> // Various wide and multibyte string handling functions
 #include <cwctype> // Functions to determine the catagory of wide characters
-#include <format> // (C++20) Formatting library including std::format
+// NOT SUPPORTED BY LINUX | #include <format> // (C++20) Formatting library including std::format
 #include <string> // std::basic_string class template
-#include <string_view> // (C++17) std::basic_string_view class template 
+// NOT SUPPORTED BY LINUX | #include <string_view> // (C++17) std::basic_string_view class template 
 // Containers library
 #include <array> // (C++11) std::array container
 #include <deque> // std::deque container
@@ -64,7 +64,7 @@
 #include <map> // std::map and std::multimap associative containers
 #include <queue> // std::queue and std::priority_queue container adaptors
 #include <set> // std::set and std::multiset associative containers
-#include <span> // (C++20) std::span view
+// NOT SUPPORTED BY LINUX | #include <span> // (C++20) std::span view
 #include <stack> // std::stack container adaptor
 #include <unordered_map> // (C++11) std::unordered_map and std::unordered_multimap unordered associative containers
 #include <unordered_set> // (C++11) std::unordered_set and std::unordered_multiset unordered associative containers
@@ -72,16 +72,16 @@
 // Iterators library
 #include <iterator> // Range iterators 
 // Ranges library
-#include <ranges> // (C++20) Range access, primitives, requirements, utilitiesand adaptors 
+// NOT SUPPORTED BY LINUX | #include <ranges> // (C++20) Range access, primitives, requirements, utilitiesand adaptors 
 // Algorithms library
 #include <algorithm> // Algorithms that operate on ranges
-#include <execution> // (C++17) Predefined execution policies for parallel versions of the algorithms 
+// NOT SUPPORTED BY LINUX | #include <execution> // (C++17) Predefined execution policies for parallel versions of the algorithms 
 // Numerics library
-#include <bit> // (C++20) Bit manipulation functions
+// NOT SUPPORTED BY LINUX | #include <bit> // (C++20) Bit manipulation functions
 #include <cfenv> // (C++11) Floating - point environment access functions
 #include <cmath> // Common mathematics functions
 #include <complex> // Complex number type
-#include <numbers> // (C++20) Math constants
+// NOT SUPPORTED BY LINUX | #include <numbers> // (C++20) Math constants
 #include <numeric> // Numeric operations on values in ranges
 #include <random> // (C++11) Random number generators and distributions
 #include <ratio> // (C++11) Compile - time rational arithmetic
@@ -98,24 +98,24 @@
 #include <iostream> // Several standard stream objects
 #include <istream> // std::basic_istream class template and several typedefs
 #include <ostream> // std::basic_ostream, std::basic_iostream class templatesand several typedefs
-#include <spanstream> // (C++23) std::basic_spanstream, std::basic_ispanstream, std::basic_ospanstream class templatesand typedefs
+// NOT SUPPORTED BY LINUX | #include <spanstream> // (C++23) std::basic_spanstream, std::basic_ispanstream, std::basic_ospanstream class templatesand typedefs
 #include <sstream> // std::basic_stringstream, std::basic_istringstream, std::basic_ostringstream class templatesand several typedefs
 #include <streambuf> // std::basic_streambuf class template
-#include <syncstream> // (C++20) std::basic_osyncstream, std::basic_syncbuf, and typedefs 
+// NOT SUPPORTED BY LINUX | #include <syncstream> // (C++20) std::basic_osyncstream, std::basic_syncbuf, and typedefs 
 // Filesystem library
-#include <filesystem> // (C++17) std::path classand supporting functions 
+// NOT SUPPORTED BY LINUX | #include <filesystem> // (C++17) std::path classand supporting functions 
 // Regular Expressions library
 #include <regex> // (C++11) Classes, algorithmsand iterators to support regular expression processing 
 // Atomic Operations library
 #include <atomic> // (C++11) Atomic operations library 
 // Thread support library
-#include <barrier> // (C++20) Barriers
+// NOT SUPPORTED BY LINUX | #include <barrier> // (C++20) Barriers
 #include <condition_variable> // (C++11) Thread waiting conditions
 #include <future> // (C++11) Primitives for asynchronous computations
-#include <latch> // (C++20) Latches
+// NOT SUPPORTED BY LINUX | #include <latch> // (C++20) Latches
 #include <mutex> // (C++11) Mutual exclusion primitives
-#include <semaphore> // (C++20) Semaphores
+// NOT SUPPORTED BY LINUX | #include <semaphore> // (C++20) Semaphores
 #include <shared_mutex> // (C++14) Shared mutual exclusion primitives
-#include <stop_token> // (C++20) Stop tokens for std::jthread
+// NOT SUPPORTED BY LINUX | #include <stop_token> // (C++20) Stop tokens for std::jthread
 #include <thread> // (C++11) std::thread classand supporting functions
 #endif
